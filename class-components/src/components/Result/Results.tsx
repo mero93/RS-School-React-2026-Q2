45ccef34dfdd4349db0d33ac7d8646205becf487
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-19 00:54:30 +0400

    refactor: convert results to functional component

--- class-components/src/components/Result/Results.tsx
+++ class-components/src/components/Result/Results.tsx
@@ -8,30 +8,26 @@ interface ResultsProps {
   items: ComicStrip[] | undefined;
 }
 
-class Results extends Component<ResultsProps> {
-  render() {
-    if (this.props.hasError) {
-      throw new Error('Simulation: Results component crashed!');
-    }
-
-    const { items } = this.props;
-
-    return (
-      <div className="results-container">
-        <h3 className="results-heading">Results Area</h3>
-
-        <div className="results-grid">
-          {!items || items.length === 0 ? (
-            <p className="results-empty-state">
-              No results found. Try a different search term.
-            </p>
-          ) : (
-            items.map((item) => <ItemCard key={item.uid} item={item} />)
-          )}
-        </div>
-      </div>
-    );
+export default function Results(props: Readonly<ResultsProps>) {
+  if (props.hasError) {
+    throw new Error('Simulation: Results component crashed!');
   }
-}
 
-export default Results;
+  const { items } = props;
+
+  return (
+    <div className="results-container">
+      <h3 className="results-heading">Results Area</h3>
+
+      <div className="results-grid">
+        {!items || items.length === 0 ? (
+          <p className="results-empty-state">
+            No results found. Try a different search term.
+          </p>
+        ) : (
+          items.map((item) => <ItemCard key={item.uid} item={item} />)
+        )}
+      </div>
+    </div>
+  );
+}
