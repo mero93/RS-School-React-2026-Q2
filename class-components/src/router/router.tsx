33fe75652ea3e42bf51102e29a62e872ac43e315
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-19 00:02:00 +0400

    feat: add empty components for pages, add router

---
+++ class-components/src/router/router.tsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from 'react-router-dom';
+import About from '../pages/About/About';
+import Home from '../pages/Home/Home';
+import NotFound from '../pages/NotFound/NotFound';
+import ItemDetails from '../components/ItemDetails/ItemDetails';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '',
+        children: [
+          {
+            path: 'details',
+            element: <ItemDetails />,
+            errorElement: <NotFound />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: 'about',
+    element: <About />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
