346eee01cff71d64e12135d9e7bcf19fd6420ef5
mero93 <merabkopaleishvili1993@gmail.com>
2026-05-18 23:02:37 +0400

    feat: add local storage hook

--- class-components/eslint.config.js
+++ class-components/eslint.config.js
@@ -25,6 +25,8 @@ export default defineConfig([
     },
     rules: {
       'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 ]);
