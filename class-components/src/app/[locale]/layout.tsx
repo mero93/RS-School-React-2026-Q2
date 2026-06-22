6e90e829594c6c364a5de1ae369c845f90941391
mero93 <merabkopaleishvili1993@gmail.com>
2026-06-22 14:27:07 +0400

    feat: add blank message objects, setup i18n provider

--- class-components/src/app/[locale]/layout.tsx
+++ class-components/src/app/[locale]/layout.tsx
@@ -3,26 +3,36 @@ import '../index.css';
 import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary';
 import Header from '../../components/Header/Header';
 import GlobalProvider from '../../providers/GlobalProvider';
+import { notFound } from 'next/navigation';
+import { getMessages } from 'next-intl/server';
 
 interface LayoutProps {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }
 
-export default async function RootLayout({ children, params }: Readonly<LayoutProps>) {
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<LayoutProps>) {
   const { locale } = await params;
+
+  const locales = ['en', 'de'];
+  if (!locales.includes(locale)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
 
   return (
     <html lang={locale || 'en'}>
-        <head></head>
+      <head></head>
       <body>
         <ErrorBoundary>
-          <GlobalProvider>
+          <GlobalProvider locale={locale} messages={messages}>
             <div className="app-layout-root">
               <Header />
-              <main className="app-content-frame">
-                {children}
-              </main>
+              <main className="app-content-frame">{children}</main>
             </div>
           </GlobalProvider>
         </ErrorBoundary>
--- class-components/src/providers/GlobalProvider.tsx
+++ class-components/src/providers/GlobalProvider.tsx
@@ -2,13 +2,24 @@ import type { ReactNode } from 'react';
 import ThemeProvider from './ThemeProvider';
 import { queryClient } from '../query-client';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { NextIntlClientProvider, type AbstractIntlMessages } from 'next-intl';
+
+interface GlobalProviderProps {
+  children: ReactNode;
+  locale: string;
+  messages: AbstractIntlMessages;
+}
 
 export default function GlobalProvider({
   children,
-}: Readonly<{ children: ReactNode }>) {
+  locale,
+  messages,
+}: Readonly<GlobalProviderProps>) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>{children}</ThemeProvider>
+      <NextIntlClientProvider locale={locale} messages={messages}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </NextIntlClientProvider>
     </QueryClientProvider>
   );
 }
